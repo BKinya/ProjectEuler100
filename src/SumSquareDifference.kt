0df40c8dec9d69c06ee2743d4bbe592bf8127b5b
kinya <gitongakinya1@gmail.comj>
2020-06-16 21:52:45 +0300

    sum square difference complete

--- src/SumSquareDifference.kt
+++ src/SumSquareDifference.kt
@@ -1,20 +1,19 @@
-fun main(){
-
+fun main() {
     findSumSquareDifference()
 }
 
-fun findSumSquareDifference(){
-    val n = readLine()!!.toLong()
-    var sumOfSquares = 0L
-    var sum = 0L
-    for(i in 1 .. n){
-        sum += i
-        sumOfSquares += (i*i);
+fun findSumSquareDifference() {
+    val t = readLine()!!.toInt()
+    for (i in 0 until t) {
+        val n = readLine()!!.toLong()
+        var sumOfSquares = 0L
+        var sum = 0L
+        for (j in 1..n) {
+            sum += j
+            sumOfSquares += (j * j);
+        }
+
+        var difference = (sum * sum) - sumOfSquares
+        println("$difference")
     }
-    println("sum => $sum")
-    println("sum of Suares => $sumOfSquares")
-
-    var difference = (sum * sum) - sumOfSquares
-    println("diff => $difference")
-
 }
